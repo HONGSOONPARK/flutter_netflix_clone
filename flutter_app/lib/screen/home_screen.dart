6c4327730263d8296303b61edd672eb4bbf91769
parkhongsoon <kongsooni39@gmail.com>
2021-03-08 01:17:23 +0900

    init

---
+++ flutter_app/lib/screen/home_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/model/model_movie.dart';
+import 'package:flutter_app/widget/box_slider.dart';
+import 'package:flutter_app/widget/carousel_slider.dart';
+import 'package:flutter_app/widget/circle_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Firestore firestore = Firestore.instance;
+  Stream<QuerySnapshot> streamData;
+
+  // List<Movie> movies = [
+  //   Movie.fromMap({
+  //     'title': '사랑의 불시착',
+  //     'keyword': '사랑/로맨스/판타지',
+  //     'poster': 'test_movie_1.png',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '사랑의 불시착',
+  //     'keyword': '사랑/로맨스/판타지',
+  //     'poster': 'test_movie_1.png',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '사랑의 불시착',
+  //     'keyword': '사랑/로맨스/판타지',
+  //     'poster': 'test_movie_1.png',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '사랑의 불시착',
+  //     'keyword': '사랑/로맨스/판타지',
+  //     'poster': 'test_movie_1.png',
+  //     'like': false,
+  //   }),
+  // ];
+
+  @override
+  void initState() {
+    super.initState();
+    streamData = firestore.collection('movie').snapshots();
+  }
+
+  Widget _fetchData(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('movie').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildBody(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    List<Movie> movies = snapshot.map((d) => Movie.fromSnapshot(d)).toList();
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarouselImage(movies: movies),
+            TopBar(),
+          ],
+        ),
+        CircleSlider(movies: movies),
+        BoxSlider(movies: movies),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _fetchData(context);
+    // return ListView(
+    //   children: <Widget>[
+    //     Stack(
+    //       children: <Widget>[
+    //         CarouselImage(movies: movies),
+    //         TopBar(),
+    //       ],
+    //     ),
+    //     CircleSlider(
+    //       movies: movies,
+    //     ),
+    //     BoxSlider(
+    //       movies: movies,
+    //     ),
+    //   ],
+    // );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset(
+            'images/bbongflix_logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
